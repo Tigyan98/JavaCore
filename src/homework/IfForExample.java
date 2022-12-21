1f6f36a244404bca01511bcbcc01b3b12ce2d581
Tigran <tigyan98@gmail.com>
2022-12-21 12:39:44 +0300

    IfForExample

---
+++ src/homework/IfForExample.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class IfForExample {
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 40;
+        if (a < b) {
+            System.out.println("max:" + b);
+        }
+
+
+    }
+}
