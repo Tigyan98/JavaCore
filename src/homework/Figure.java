77ea7b9b81062da6a2bbb3d2b10ba08be1719f81
Tigran <tigyan98@gmail.com>
2022-12-21 23:39:14 +0300

    IfForExample

--- src/homework/Figure.java
+++ src/homework/Figure.java
@@ -3,12 +3,17 @@ package homework;
 public class Figure {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
+        for (int i = 0; i < 5 ; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print("* " );
 
-            }
+
+                }
             System.out.println();
+             }
+
+
         }
+
     }
-}
+
--- src/homework/IfForExample.java
+++ src/homework/IfForExample.java
@@ -3,10 +3,33 @@ package homework;
 public class IfForExample {
     public static void main(String[] args) {
         int a, b;
-        a = 10;
-        b = 40;
+        a = 5;
+        b = 10;
+
         if (a < b) {
             System.out.println("max:" + b);
+        } else {
+            System.out.println("max:" + a);
+        }
+
+        for (int i = a; i <= b; i++) {
+            System.out.println(i);
+        }
+
+
+        char c = (char) a;
+        char c1 = (char) b;
+        System.out.println(c);
+        System.out.println(c1);
+
+        if (a==b) {
+            System.out.println("a havasare b");
+
+        }else {
+            System.out.println("a havasar che b");
+
+        }
         }
     }
-}
+
+
