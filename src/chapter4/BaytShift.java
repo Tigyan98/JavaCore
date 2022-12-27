cf145b2be83d0c45017f78a26096f7f8d479239d
Tigran <tigyan98@gmail.com>
2022-12-28 00:45:15 +0300

    IfForExample

---
+++ src/chapter4/BaytShift.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+public class BaytShift {
+    public static void main(String[] args) {
+        byte a = 64, b;
+        int i;
+        i = a << 2;
+        b = (byte) (a << 2);
+
+        System.out.println("первоначальное значение a: " + a);
+        System.out.println("i and b:" + i + " " + b);
+    }
+}
--- src/homework2/ArrayUtil.java
+++ src/homework2/ArrayUtil.java
@@ -2,42 +2,77 @@ package homework2;
 
 public class ArrayUtil {
     public static void main(String[] args) {
-
         int[] array = {2, 5, 11, 25, 66, 10, 62, 46, 3, 7};
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-            if (i != array.length-1){
-                System.out.print(",");
-            }else {
-                System.out.print(".");
-            }
-
-        }
-        System.out.println();
-
-        int min = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-            }
-        }
-        System.out.println("amena poqr: " + min);
-
-        int max = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
-            }
-        }
-        System.out.println("amena mec tiv:" + max);
-
-
-        System.out.println("arajin tivy: " + array[0]);
-        int verjiIndex = array.length - 1;
-        System.out.println("verjin tivy: " + array[verjiIndex]);
-
-
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 == 0) {
+//
+//                count += 1;
+//
+//                System.out.print(count + " ");
+//            }
+//
+//        }
+//        System.out.println();
+//
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0) {
+//
+//                count += 1;
+//                System.out.print(count + " ");
+//            }
+//
+//        }
+//
+//        System.out.println();
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0) {
+//                System.out.print(array[i] + " ");
+//            }
+//
+//        }
+//        System.out.println();
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 == 0) {
+//                System.out.print(array[i] + " ");
+//            }
+//
+//        }
+//
+//
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.print(array[i]);
+//            if (i != array.length - 1) {
+//                System.out.print(",");
+//            } else {
+//                System.out.print(".");
+//            }
+//
+//        }
+//        System.out.println();
+//
+//        int min = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] < min) {
+//                min = array[i];
+//            }
+//        }
+//        System.out.println("amena poqr: " + min);
+//
+//        int max = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] > max) {
+//                max = array[i];
+//            }
+//        }
+//        System.out.println("amena mec tiv:" + max);
+//
+//
+//        System.out.println("arajin tivy: " + array[0]);
+//        int verjiIndex = array.length - 1;
+//        System.out.println("verjin tivy: " + array[verjiIndex]);
     }
-
 }
-
